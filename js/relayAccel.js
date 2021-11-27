7e9a898c0bd7276240047e988d2f02e76c23ee3e
Yuki <yukiharuka@outlook.com>
2021-11-27 13:58:30 +0900

    Export time with data of sensor

--- js/relayAccel.js
+++ js/relayAccel.js
@@ -4,7 +4,8 @@ const accelMotor__From__RasPI = "accelMotor__From__RasPI";
 // 怖いがっている
 const SCARY_LIMIT = 15;
 
-let data = new Array();
+let sensorData = new Array();
+let date = new Array();
 
 let channel;
 async function setRelay()
@@ -17,21 +18,28 @@ async function setRelay()
 
 async function getMessage(msg)
 {
-    console.log(msg.data);
-    let values = msg.data;
+    // データ取得
+    let data = msg.data;
     let value = document.getElementById("valueAccel");
     let scary = document.getElementById("scaryAccel");
-    let isScary = (values.all > SCARY_LIMIT) ? true : false;
-    value.innerHTML = values.all;
-    scary.innerHTML = (isScary === true) ? "O" : "X";
+    let isScary = (sensorData.all > SCARY_LIMIT) ? true : false;
+    value.innerHTML = sensorData.all;                               // 「値」出力
+    scary.innerHTML = (isScary === true) ? "O" : "X";           // 「怖いか」 出力
+    
+    let max = Math.max.apply(null, sensorData);                       // 最大値取得
+    let maxAccel = document.getElementById("maxAccel");
+    maxAccel.innerHTML = max;                                   // 最大値出力
 
     let arrayAccel = document.getElementById("arrayAccel");
-    data.push(values.all);
-    let tr = document.createElement("tr");
-    let th = document.createElement("th");
+    sensorData.push(data.all);                                      // 配列に値を追加
+    let tr = document.createElement("tr");                      // tableに値を出力
     let td = document.createElement("td");
-    td.innerHTML = values.all;
+    let th = document.createElement("th");
+    let now = new Date();                                       // 現在時刻取得
+    date.push(`${now.getFullYear()}/${now.getMonth()}/${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+    th.innerHTML = `${now.getFullYear()}/${now.getMonth()}/${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    td.innerHTML = data.all;
+    tr.appendChild(th);
     tr.appendChild(td);
     arrayAccel.appendChild(tr);
-    // arrayAccel.innerHTML += `${values.all}\n`;
 }
