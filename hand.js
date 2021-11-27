7fa3c5edb449ae4561e80ee017335998adce04c8
Yuki <yukiharuka@outlook.com>
2021-11-27 14:07:18 +0900

    Change hand.js to remote version

--- hand.js
+++ hand.js
@@ -1,7 +1,11 @@
 import {requestI2CAccess} from "./node_modules/node-web-i2c/index.js";
+import nodeWebSocketLib from "websocket";
+import {RelayServer} from "./RelayServer.js.js";
 import ADS1X15 from "@chirimen/ads1x15";
 import SHT30 from "@chirimen/sht30";
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+const sht30__From__RasPI = "sht30__From__RasPI";
 
 main();
 
@@ -20,14 +24,22 @@ async function main() {
       const rawData = await ads1x15.read(0);
       const voltage = ads1x15.getVoltage(rawData);
       output += `CH${0}:${voltage.toFixed(3)}V `;
-    await sleep(500);
-    const { humidity, temperature } = await sht30.readData();
-    console.log(
+      await sleep(500);
+      const data = {
+        humidity: undefined,
+        temperature: undefined,
+      };
+      const data = await sht30.readData();
+      console.log(
       [
         `Humidity: ${humidity.toFixed(2)}%`,
         `Temperature: ${temperature.toFixed(2)} degree`
       ].join(", ")
-    );
-    console.log(output);
+      );
+      console.log(output);
+      
+      let relay = RelayServer("achex", "chirimenSocket" , nodeWebSocketLib, "https://chirimen.org");
+      let channel = relay.subscribe(sht30__From__RasPI);
+      channel.send(data.temperature);
     }
-}
+  }
