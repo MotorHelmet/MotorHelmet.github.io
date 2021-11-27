85dfe5311f8c21964faff2f2fb10e65b6b955eb9
Yuki <yukiharuka@outlook.com>
2021-11-27 14:58:14 +0900

    Collect isScary

--- js/relayAccel.js
+++ js/relayAccel.js
@@ -4,8 +4,10 @@ const accelMotor__From__RasPI = "accelMotor__From__RasPI";
 // 怖いがっている
 const SCARY_LIMIT = 15;
 
+// 記録用の配列
 let sensorData = new Array();
 let date = new Array();
+let isScarryData = new Array();
 
 let channel;
 async function setRelay()
@@ -22,24 +24,36 @@ async function getMessage(msg)
     let data = msg.data;
     let value = document.getElementById("valueAccel");
     let scary = document.getElementById("scaryAccel");
-    let isScary = (sensorData.all > SCARY_LIMIT) ? true : false;
-    value.innerHTML = sensorData.all;                               // 「値」出力
+    value.innerHTML = data.all;                               // 「値」出力
+
+    let arrayAccel = document.getElementById("arrayAccel");
+    sensorData.push(data.all);                                  // 配列に値を追加
+
+    let isScary = (data.all > SCARY_LIMIT) ? true : false;
     scary.innerHTML = (isScary === true) ? "O" : "X";           // 「怖いか」 出力
+    isScarryData.push(isScary);
     
-    let max = Math.max.apply(null, sensorData);                       // 最大値取得
+    // let max = Math.max.apply(null, sensorData);                       // 最大値取得
+    let max = sensorData.reduce(function(a, b)
+    {
+        return (a <= b) ? b : a;
+    });
+    console.log(max);
     let maxAccel = document.getElementById("maxAccel");
     maxAccel.innerHTML = max;                                   // 最大値出力
 
-    let arrayAccel = document.getElementById("arrayAccel");
-    sensorData.push(data.all);                                      // 配列に値を追加
-    let tr = document.createElement("tr");                      // tableに値を出力
-    let td = document.createElement("td");
-    let th = document.createElement("th");
     let now = new Date();                                       // 現在時刻取得
     date.push(`${now.getFullYear()}/${now.getMonth()}/${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+
+    let tr = document.createElement("tr");                      // tableに値を出力
+    let tdData = document.createElement("td");
+    let tdIsScaryData = document.createElement("td");
+    let th = document.createElement("th");
     th.innerHTML = `${now.getFullYear()}/${now.getMonth()}/${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-    td.innerHTML = data.all;
+    tdData.innerHTML = data.all;
+    tdIsScaryData.innerHTML = (isScary === true) ? "O" : "X";
     tr.appendChild(th);
-    tr.appendChild(td);
+    tr.appendChild(tdData);
+    tr.appendChild(tdIsScaryData);
     arrayAccel.appendChild(tr);
 }
